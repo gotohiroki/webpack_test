7c6f272b3f258efff71a0764678c8cba7104e240
hiroki <57242780+gotohiroki@users.noreply.github.com>
2022-08-12 19:23:13 +0900

    Improve file structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
+    filename: './js/main.js',
   },
   module: {
     rules: [
@@ -24,10 +25,13 @@ module.exports = {
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "./css/main.css",
+    }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new CleanWebpackPlugin(),
   ],
 }
 
