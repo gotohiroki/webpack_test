5c1dda3aaac4b1f2fb4ecbc13ad2e8d6637c8cbd
hiroki <57242780+gotohiroki@users.noreply.github.com>
2022-08-12 20:11:56 +0900

    Use Asset Modules

--- webpack.config.js
+++ webpack.config.js
@@ -24,14 +24,18 @@ module.exports = {
       },
       {
         test: /\.(png|jpg)/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'image/[name][ext]'
+        },
         use: [
-          {
-            loader: 'file-loader',
-            options: {
-              esModule: false,
-              name: 'image/[name].[ext]'
-            }
-          },
+          // {
+          //   loader: 'file-loader',
+          //   options: {
+          //     esModule: false,
+          //     name: 'image/[name].[ext]'
+          //   }
+          // },
         ],
       },
     ],
